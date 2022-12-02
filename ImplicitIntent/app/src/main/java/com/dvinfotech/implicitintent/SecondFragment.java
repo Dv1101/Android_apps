d0622d99bc7d3567f19927b7790781e4cb59b304
Dv1101 <dhruvvaria1101@gmail.com>
2022-12-02 13:31:07 +0530

    Message Intent integration into second fragement

--- ImplicitIntent/app/src/main/java/com/dvinfotech/implicitintent/SecondFragment.java
+++ ImplicitIntent/app/src/main/java/com/dvinfotech/implicitintent/SecondFragment.java
@@ -1,9 +1,14 @@
 package com.dvinfotech.implicitintent;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -13,6 +18,8 @@ import com.dvinfotech.implicitintent.databinding.FragmentSecondBinding;
 
 public class SecondFragment extends Fragment {
 
+    Button button2;
+    EditText msg;
     private FragmentSecondBinding binding;
 
     @Override
@@ -34,6 +41,27 @@ public class SecondFragment extends Fragment {
             public void onClick(View view) {
                 NavHostFragment.findNavController(SecondFragment.this)
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
+            }
+        });
+
+        button2 = view.findViewById(R.id.button2);
+        msg = view.findViewById(R.id.msg);
+        button2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String msgText = msg.getText().toString();
+                Toast.makeText(getActivity(), msgText, Toast.LENGTH_SHORT).show();
+
+                String [] addresses = {"dhruvvaria1101@gmail.com", "droidtv1101@gmail.com"};
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("*/*");
+                intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Dv's implicit intent apps subject");
+                intent.putExtra(Intent.EXTRA_TEXT, msgText);
+                if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+                    startActivity(intent);
+                }
             }
         });
     }
