146479ea468eb7e95d05ef3b10964930a0b79ea3
Dv1101 <dhruvvaria1101@gmail.com>
2022-12-16 18:36:31 +0530

    Broadcast reciever applicatiion -for airplane mode

---
+++ AirplaneMode_BroadcastReciever/app/src/main/java/com/dvinfotech/airplanemode_broadcastreciever/MainActivity.java
@@ -0,0 +1,30 @@
+package com.dvinfotech.airplanemode_broadcastreciever;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    MyReciever myReciever = new MyReciever();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter filter = new IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        registerReceiver(myReciever, filter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(myReciever);
+    }
+}
